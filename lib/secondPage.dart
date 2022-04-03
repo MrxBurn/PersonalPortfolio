2ef29355b470b2244393ead3da4d71b9c21a4896
Alex Todea <xburngaming@gmail.com>
2022-04-03 19:11:49 +0100

    ListView Builder

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:personal_portfolio/firstPage.dart';
+import 'dart:math';
+import 'package:animate_do/animate_do.dart';
 
 class Description extends StatefulWidget {
   const Description({Key? key}) : super(key: key);
@@ -11,17 +13,44 @@ class Description extends StatefulWidget {
 }
 
 class _DescriptionState extends State<Description> {
+  //Variables
+  List<String> titles = ['DSUK App', 'Cyber Training App'];
+  List<String> descriptions = ['Durham Constabulary', 'Dissertation'];
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('Hello Page 2'),
-        IconButton(
-            onPressed: () {
-              HomePage.pageController.previousPage(
-                  duration: Duration(seconds: 1), curve: Curves.easeInOut);
-            },
-            icon: Icon(Icons.arrow_upward_rounded))
+        Align(
+            alignment: Alignment.topLeft,
+            child: IconButton(
+              icon: Transform.rotate(
+                  angle: 90 * pi / 180,
+                  child: Pulse(
+                      infinite: true,
+                      child: Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.white.withOpacity(0.3),
+                      ))),
+              iconSize: 30,
+              hoverColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              onPressed: () {
+                HomePage.pageController.previousPage(
+                    duration: Duration(seconds: 1), curve: Curves.easeInOut);
+              },
+            )),
+        SizedBox(
+            width: 400,
+            height: 600,
+            child: ListView.builder(
+                itemCount: titles.length,
+                itemBuilder: ((context, index) {
+                  return ListTile(
+                    title: Text(titles[index]),
+                    subtitle: Text(descriptions[index]),
+                  );
+                })))
       ],
     );
   }
